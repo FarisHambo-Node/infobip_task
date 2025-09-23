bcae56ddf4e2747f89e915d9a04888b780851ccf
Faris Hambo <faris.hambo@nodeitx.com>
2025-09-23 09:02:41 +0200

    Feat: Applying terraform so dummy data can be created

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,17 +1,14 @@
 variable "aws_region" {
   description = "AWS region for resources"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "project_name" {
   description = "Name of the project"
   type        = string
-  default     = "data-engineering-task"
 }
 
 variable "environment" {
   description = "Environment name (dev, staging, prod)"
   type        = string
-  default     = "dev"
 }
